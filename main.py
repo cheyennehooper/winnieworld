83eadf7ec6ed510f2644fa8f02918b59a8294cd4
cheyennehooper <hooptown52@gmail.com>
2025-05-16 07:11:47 -0400

    tile setup

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ import sys
 #init game
 pygame.init()
 
+#tile setup
+tile_size = 32
+tiles_x = 20
+tiles_y = 15
+
 #display
-WIDTH, HEIGHT = 640, 480
+WIDTH, HEIGHT = tile_size * tiles_x, tile_size * tiles_y
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("winnies world")
 
@@ -13,14 +18,41 @@ pygame.display.set_caption("winnies world")
 winnie_img = pygame.image.load("assets/winnie.png")
 winnie_rect = winnie_img.get_rect(center=(WIDTH//2, HEIGHT//2))
 
+#frame speed
+SPEED = 3
+
 #game loop
+clock = pygame.time.Clock()
 running = True
 while running:
+    clock.tick(60)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((225,225,225))
+    #keymapping the motion
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        winnie_rect.x -= SPEED
+    if keys[pygame.K_RIGHT]:
+        winnie_rect.x += SPEED    
+    if keys[pygame.K_UP]:
+        winnie_rect.y -= SPEED
+    if keys[pygame.K_DOWN]:
+        winnie_rect.y += SPEED
+
+    #tile board temp
+    for row in range(tiles_y):
+        for col in range(tiles_x):
+            color = (158,207,141) if (row + col) % 2 == 0 else (109,185,102)
+            rect = pygame.Rect(col * tile_size, row * tile_size, tile_size, tile_size)
+            pygame.draw.rect(screen, color, rect)
+
+
+
+
+    #drawing on screen
     screen.blit(winnie_img, winnie_rect)
     pygame.display.flip()
 
